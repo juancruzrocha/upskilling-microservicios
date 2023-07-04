e25db8228fe6ead68f59ccccfce6512063262600
Cruz Rocha <juancruzrocha@live.com>
2023-07-04 13:10:09 -0300

    small changes
    i just changed 3 lines of code as a examples fotr a youtube video.
    - /planets/src/routes/index.js

--- star-wars-api-micro-servicies/planets/src/routes/index.js
+++ star-wars-api-micro-servicies/planets/src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const controllers = require("../controllers");
-const middlewares = require("../middlewares");
+//const middlewares = require("../middlewares");
 
 const router = Router();
 
 router.get("/", controllers.getPlanets);
-router.post("/", middlewares.filmValidation, controllers.createPlanet);
+router.post("/", controllers.createPlanet);
+//router.post("/", middlewares.filmValidation, controllers.createPlanet);
 
 module.exports = router;
